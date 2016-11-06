0ef6a45db0343c2d6487f3c9b9799fa532ebec89
Akshar Patel <akshar.patel.47@gmail.com>
2016-11-06 19:35:14 +0530

    feat: Add newly created routes to the app

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 import * as express from 'express';
 import UserRoutes from "./user/user.routes";
+import SessionRoutes from './session/session.routes';
+import OpportunitiesRoutes from './opportunities/opportunities.routes';
+import EventsRoutes from './events/events.routes';
 
 const app = express();
 
@@ -7,5 +10,8 @@ const app = express();
 
 // Add Routes Here...
 app.use('/user', UserRoutes);
+app.use('/session', SessionRoutes);
+app.use('/opportunities', OpportunitiesRoutes);
+app.use('/events', EventsRoutes);
 
 export default app;
