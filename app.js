613b784f4730a6f435376451b06e1f449e7f80ca
Akshar Patel <akshar.patel.47@gmail.com>
2016-11-20 22:50:59 +0530

    feat: Add account routes to the app

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const accountRoutes = require('./account/account.routes');
 
 const app = express();
 
 // Routes
-
+app.use('/account', accountRoutes);
 
 module.exports = app;
