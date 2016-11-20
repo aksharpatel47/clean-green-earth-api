2ff182323abd2d756b94072cd88d5f7da5c950e3
Akshar Patel <akshar.patel.47@gmail.com>
2016-11-20 22:48:11 +0530

    refactor: Remove typescript from project

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import app from './app';
-
-const port: number = process.env.PORT || 8101;
-
-app.listen(port, (err) => {
-  if (err) throw err;
-  console.log('Server listening on port', port);
-});
--- src/session/session.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-
-const SessionRoutes = Router();
-
-SessionRoutes
-  .post('/:email', (req, res) => res.sendStatus(200))
-  .delete('/:email', (req, res) => res.sendStatus(200));
-
-export default SessionRoutes;
