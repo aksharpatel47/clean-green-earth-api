d633996649bb1b198b8df8afde7b8dcf524bf780
Akshar Patel <akshar.patel.47@gmail.com>
2016-11-22 01:28:14 +0530

    feat: Add body-parser to parse json

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const accountRoutes = require('./account/account.routes');
 const authenticationMiddleware = require('./middleware/authentication.middleware');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 // Middlewares
+app.use('/', bodyParser.json());
 app.use('/', authenticationMiddleware);
 
 // Routes
