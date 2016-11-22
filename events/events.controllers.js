1b55b0223f739c75638db2cfcaaca8796b1eac64
Akshar Patel <akshar.patel.47@gmail.com>
2016-11-22 15:59:28 +0530

    feat: implement event apis

--- events/events.controllers.js
+++ events/events.controllers.js
@@ -1,11 +1,59 @@
-exports.createEvent = function createEvent(req, res) {
+const db = require('../db');
+const uuid = require('uuid');
 
+exports.createEvent = function createEvent(req, res) {
+  const { uid, title, description, location, dateInSeconds, startTimeInSeconds, endTimeInSeconds } = req.body;
+  const { latitude, longitude } = location;
+
+  const eventId = uuid.v4();
+  const date = new Date(dateInSeconds);
+  const startTime = new Date(startTimeInSeconds);
+  const endTime = new Date(endTimeInSeconds);
+
+  const query = `insert into events(id, title, description, location, date, start_time, end_time, user_id)
+  values($1, $2, $3, $4, $5, $6, $7, $8)`;
+  const values = [eventId, title, description, `(${longitude},${longitude})`, date, startTime, endTime, uid];
+
+  db.none(query, values)
+    .then(() => {
+      res.status(201).json({data: {message: 'success'}});
+    }, (err) => {
+      res.status(400).json({data: err});
+    })
 }
 
 exports.updateEvent = function updateEvent(req, res) {
+  const eventId = req.params.id;
+  const { uid, title, description, location, dateInSeconds, startTimeInSeconds, endTimeInSeconds } = req.body;
 
+  const { latitude, longitude } = location;
+  const date = new Date(dateInSeconds);
+  const startTime = new Date(startTimeInSeconds);
+  const endTime = new Date(endTimeInSeconds);
+
+  const query = `update events set title = $1, description = $2, location = $3, date = $4, start_time = $5, 
+    end_time = $6 where id = $7 and user_id = $8`;
+  const values = [title, description, `(${longitude},${latitude})`, date, startTime, endTime, uid, eventId]
+
+  db.none(query, values)
+    .then(() => {
+      res.status(200).json({data: {message: 'success'}});
+    }, (err) => {
+      res.status(400).json({data: err});
+    });
 }
 
 exports.deleteEvent = function deleteEvent(req, res) {
+  const eventId = req.params.id;
+  const { uid } = req.body;
 
+  const query = 'delete from events where uid = $1 and id = $2';
+  const values = [uid, eventId];
+
+  db.none(query, values)
+    .then(() => {
+      res.status(200).json({data: {message: 'success'}});
+    }, (err) => {
+      res.status(400).json({data: err});
+    });
 }
