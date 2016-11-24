6294e60807ded103c7d520bf3942b9be1dd5a525
Akshar Patel <akshar.patel.47@gmail.com>
2016-11-24 16:37:09 +0530

    refactor: Handle event att. routes in event router

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ app.use(require('./middleware/authentication.middleware'));
 // Routes
 app.use('/users', require('./users/users.routes'));
 app.use('/events', require('./events/events.routes'));
-app.use('/events', require('./attendance/attendance.routes'));
+// app.use('/events', require('./attendance/attendance.routes'));
 
 module.exports = app;
--- events/events.routes.js
+++ events/events.routes.js
@@ -1,6 +1,6 @@
 const Router = require('express').Router;
 const controllers = require('./events.controllers');
-const attControllers = require('../attendance/attendance.controllers');
+const attRoutes = require('../attendance/attendance.routes');
 
 const eventRoutes = Router();
 
@@ -10,5 +10,7 @@ eventRoutes
   .post('/', controllers.createEvent)
   .put('/:id', controllers.updateEvent)
   .delete('/:id', controllers.deleteEvent);
+
+eventRoutes.use('/', attRoutes);
 
 module.exports = eventRoutes
