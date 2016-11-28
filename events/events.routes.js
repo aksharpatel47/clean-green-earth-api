a10a1af4fc8cadb6659d686296cc588e47976940
Akshar Patel <akshar.patel.47@gmail.com>
2016-11-28 18:29:49 +0530

    feat: Add image middleware to event & user routes

--- events/events.routes.js
+++ events/events.routes.js
@@ -1,13 +1,14 @@
 const Router = require('express').Router;
 const controllers = require('./events.controllers');
 const attRoutes = require('../attendance/attendance.routes');
+const eventImageUpload = require('../middleware/image-upload.middleware').eventImageUpload;
 
 const eventRoutes = Router();
 
 eventRoutes
   .get('/', controllers.searchEvents)
   .get('/:id', controllers.getEventDetails)
-  .post('/', controllers.createEvent)
+  .post('/', eventImageUpload.single('event-image'), controllers.createEvent)
   .put('/:id', controllers.updateEvent)
   .delete('/:id', controllers.deleteEvent);
 
--- users/users.routes.js
+++ users/users.routes.js
@@ -1,11 +1,12 @@
 const Router = require('express').Router;
 const controllers = require('./users.controllers');
+const userImageUpload = require('../middleware/image-upload.middleware').userImageUpload;
 
 const userRoutes = Router();
 
 userRoutes
   .post('/', controllers.createUser)
-  .patch('/', controllers.updateUserDetails)
+  .patch('/', userImageUpload, controllers.updateUserDetails)
   .get('/id', controllers.getUserDetails)
   .get('/:id/events', controllers.getUserEvents)
   .get('/:id/attendance', controllers.getEventsWithUserAttendance);
