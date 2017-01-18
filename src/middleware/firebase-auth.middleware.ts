5a62d3ee77969f878f31f57abfb5158f208c29da
Akshar Patel <akshar.patel.47@gmail.com>
2017-01-18 09:44:50 +0530

    fix: require auth token for every api call

--- src/middleware/firebase-auth.middleware.ts
+++ src/middleware/firebase-auth.middleware.ts
@@ -1,29 +1,26 @@
 import firebaseAdmin from '../firebase-admin';
 import { Request, Response } from 'express';
+import { Auth } from 'firebase-admin/lib/auth/auth';
 
-export default function firebaseAuthMiddleware(req: Request, res: Response, next: any): any {
-  if (req.path === "/users" && req.method === "POST") {
-    return next()
-  }
-
-  let authorizationToken = req.get('Authorization');
+export default function firebaseAuthMiddleware(req: Request, res: Response, next: any) {
+  const authorizationToken = req.get('Authorization');
 
   if (!authorizationToken) {
     return res.sendStatus(401);
   }
 
-  let tokenComponents = authorizationToken.split(' ');
+  const tokenComponents = authorizationToken.split(' ');
 
-  if (tokenComponents.length != 2) {
+  if (tokenComponents.length !== 2 && tokenComponents[0] !== 'Bearer') {
     return res.sendStatus(401);
   }
 
-  let token = tokenComponents[1];
+  const token = tokenComponents[1];
 
-  (<any>firebaseAdmin.auth()).verifyIdToken(token)
+  (firebaseAdmin.auth() as Auth).verifyIdToken(token)
     .then((decodedToken: any) => {
-      req.body.uid = decodedToken.uid;
-      next()
+      req['user'] = decodedToken;
+      next();
     })
     .catch((err: any) => {
       return res.status(401).send(err);
